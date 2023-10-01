eb75e2049d8438e260eb0e738584728f0865531e
None <None>
2023-10-01 07:39:50 -0500

    Add flexible port

--- application.py
+++ application.py
@@ -1,19 +1,19 @@
-# Import necessary libraries
 import os
 import requests
 import dash
-from dash import dcc, html, Input, Output, dash_table  # changed dash_table import here
+from dash import dcc, html, Input, Output, dash_table
 import pandas as pd
 import dash_bootstrap_components as dbc
 import threading
 
+# Global Variables and Data Loading
 filename = "AlphaMissense_hg38.tsv.gz"
 url = "https://zenodo.org/record/8208688/files/AlphaMissense_hg38.tsv.gz?download=1"
 data_ready_event = threading.Event()
 
+# Function to download and load the data in a separate thread
 def download_and_load_data():
     global df
-    
     print("Downloading AlphaMissense table...")
 
     if not os.path.exists(filename):
@@ -22,7 +22,7 @@ def download_and_load_data():
             for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
                     file.write(chunk)
-
+                    
         if os.path.exists(filename):
             print(f"{filename} has been downloaded.")
         else:
@@ -30,101 +30,76 @@ def download_and_load_data():
     else:
         print(f"{filename} already exists.")
     
-    print("Current Working Directory:", os.getcwd())
-    print("Files in the Current Directory:", os.listdir(os.getcwd()))
     print("Loading AlphaMissense table...")
     df = pd.read_csv(filename, sep="\t", compression='gzip', skiprows=3).rename(columns={"#CHROM": "CHROM"})
     print("AlphaMissense table loaded.")
-    
     data_ready_event.set()
 
+# Start the data loading thread
 threading.Thread(target=download_and_load_data).start()
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# Define the primary layout
+app.layout = html.Div([
+    html.Div(id='table-container'),
+    dcc.Interval(id='data-check-interval', interval=2*1000, max_intervals=30)  # check every 2 seconds for 30 times
+])
 
-app.layout = dbc.Container([
-    dbc.Row(dbc.Col(html.H1("AlphaMissense Lookup"), width={"size": 6, "offset": 3}), className="mb-4"),
-    dbc.Row([
-        dbc.Col([
-            dbc.Label('Chromosome:'),
-            dbc.Select(
-                id="chrom-dropdown",
-                options=[],
-                value=None
-            )
-        ], width=2),
-        dbc.Col([
-            dbc.Label('Position:'),
-            dbc.Input(id="position-input", type="number", placeholder="Enter position")
-        ], width=2),
-        dbc.Col([
-            dbc.Label('Genotype:'),
-            dbc.Input(id="genotype-input", type="text", placeholder="Enter genotype")
-        ], width=2),
-        dbc.Col([
-            dbc.Button("Submit", id="submit-btn", color="primary", className="mt-4")
-        ], width=2)
-    ], className="mb-4"),
-    dbc.Row([
-        dbc.Col(
-            dcc.Loading(
-                id="loading",
-                type="default",
-                children=[
-                    dash_table.DataTable(id="table-output")
-                ]
-            )
-        )
-    ])
-], fluid=True)
-
-app.clientside_callback(
-    """
-    function(n_submit, n_enter) {
-        if(n_enter>n_submit) {
-            return n_enter;
-        } else {
-            return n_submit;
-        }
-    }
-    """,
-    Output('submit-btn', 'n_clicks'),
-    Input('submit-btn', 'n_clicks'),
-    Input('genotype-input', 'n_submit')
+# Callback to update the layout based on data availability
+@app.callback(
+    Output('table-container', 'children'),
+    [Input('data-check-interval', 'n_intervals')]
 )
+def update_layout(n):
+    if data_ready_event.is_set():
+        return [
+            dbc.Container([
+                dbc.Row(dbc.Col(html.H1("AlphaMissense Lookup"), width={"size": 6, "offset": 3}), className="mb-4"),
+                dbc.Row([
+                    # Dropdown, Input, and Button components go here
+                    # ... 
+                ]),
+                dbc.Row([
+                    dbc.Col(
+                        dcc.Loading(
+                            id="loading",
+                            type="default",
+                            children=[
+                                dash_table.DataTable(id="table-output", columns=[{"name": col, "id": col} for col in df.columns])
+                            ]
+                        )
+                    )
+                ])
+            ])
+        ]
+    elif n >= 30:
+        return html.Div("Failed to load data within time limit.")
+    else:
+        return html.H1('Loading AlphaMissense data... Please wait.')
 
+# Callback to update table (if you have other callbacks, add them as needed)
 @app.callback(
     Output("table-output", "data"),
-    Output("chrom-dropdown", "options"),
-    Output("chrom-dropdown", "value"),
     [Input("submit-btn", "n_clicks")],
     [dash.dependencies.State("chrom-dropdown", "value"),
      dash.dependencies.State("position-input", "value"),
      dash.dependencies.State("genotype-input", "value")]
 )
 def update_table(n_clicks, chrom, position, genotype):
-    data_ready_event.wait()
-    options = sorted(
-                [{'label': chrom, 'value': chrom} for chrom in df['CHROM'].unique()],
-                key=lambda x: (int(x['label'][3:]) if x['label'][3:].isdigit() else float('inf'), x['label'])
-            )
-    initial_value = df['CHROM'].unique()[0] if not chrom else chrom
-
-    if not genotype:
-        return [], options, initial_value
+    if not genotype or not data_ready_event.is_set():
+        return []
 
     genotype = genotype.upper()
-
     if len(genotype) == 1:
         filtered_df = df[(df['CHROM'] == chrom) & (df['POS'] == position) & 
                          (df['ALT'] == genotype)]
     else:
         filtered_df = df[(df['CHROM'] == chrom) & (df['POS'] == position) & 
                          ((df['ALT'] == genotype[0]) | (df['ALT'] == genotype[1]))]
+    return filtered_df.to_dict('records')
 
-    return filtered_df.to_dict('records'), options, initial_value
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8050))  # Get port from environment or default to 8050 for local
-    app.run_server(debug=False, host='0.0.0.0', port=port)  # Make sure debug is set to False and bind to all IPs
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))  # 8050 is a default port you choose for local development
+    app.run_server(debug=False, host='0.0.0.0', port=port)
